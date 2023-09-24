5a0965bd40c418a8a62f01b3704b78e934510281
MajotraderLucky <majohead@gmail.com>
2023-09-24 18:08:22 +0300

    feat(views): Add FileUploadView class

--- file_api/uploadfile_api_/views.py
+++ file_api/uploadfile_api_/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .models import File
 from .serializers import FileSerializer
 
 class FileView(generics.ListCreateAPIView):
   queryset = File.objects.all()
   serializer_class = FileSerializer
+
+class FileUploadView(APIView):
+  def post(self, request):
+    file_serializer = FileSerializer(data=request.data)
+    if file_serializer.is_valid():
+      file_serializer.save()
+      return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+    else:
+      return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
