1d6b24c268f96d47608ad8a615a09cc792793211
Sergey Ryazanov <majo78@mail.ru>
2023-09-25 17:46:45 +0300

    feat(urls.py): Add path /upload

--- file_api/uploadfile_api_/urls.py
+++ file_api/uploadfile_api_/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import FileView
+from .views import FileView, FileUpload
 
 urlpatterns = [
     path('files/', FileView.as_view(), name='file-list'),
+    path('upload/', FileUpload.as_view(), name='file-upload'),
 ]
