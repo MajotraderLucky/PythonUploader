a5253cf709662ad605e73bb14bccca0e18403e82
MajotraderLucky <majohead@gmail.com>
2023-09-26 18:09:33 +0300

    feat(media): Add media folder for the upload files

--- file_api/file_api/settings.py
+++ file_api/file_api/settings.py
@@ -128,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CELERY_BROKER_URL ='redis://localhost:6379/0'
 CELERY_RESULT_BACKEND ='redis://localhost:6379/0'
+
+MEDIA_ROOT = '/var/www/media'
+MEDIA_URL = '/media/'
