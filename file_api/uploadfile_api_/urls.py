0ac1a71b90dcac043050621937dc328c4e5db97e
MajotraderLucky <majohead@gmail.com>
2023-09-27 11:19:46 +0300

    feat(index): Add button for the upload files

--- file_api/uploadfile_api_/urls.py
+++ file_api/uploadfile_api_/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import FileView, FileUploadView
+from .views import FileView, FileUploadView, index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('files/', FileView.as_view(), name='file-list'),
     path('upload/', FileUploadView.as_view(), name='file-upload'),
 ]
--- file_api/uploadfile_api_/views.py
+++ file_api/uploadfile_api_/views.py
@@ -22,3 +22,6 @@ class FileUploadView(APIView):
       return Response(file_serializer.data, status=status.HTTP_201_CREATED)
     else:
       return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+def index(request):
+  return render(request, 'index.html')
