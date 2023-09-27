650a07796d80ca608197c03924ba76b3aa59bdd2
MajotraderLucky <majohead@gmail.com>
2023-09-27 21:10:39 +0300

    test(models): Add unit-test for app models

--- file_api/uploadfile_api_/tests.py
+++ file_api/uploadfile_api_/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import File
 
-# Create your tests here.
+
+class FileModelTest(TestCase):
+  def test_file_atrributes(self):
+      file = File(file='test.txt')
+      self.assertEqual(file.file, 'test.txt')
+      self.assertFalse(file.processed)
+
+  def test_file_upload(self):
+      file = File(file='test.txt')
+      file.save()
+      self.assertIsNotNone(file.uploaded_at)
+
+  def test_file_processed(self):
+      file = File(file='test.txt')
+      file.processed = True
+      self.assertTrue(file.processed)
